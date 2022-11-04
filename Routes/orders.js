c96c763fdffdd275892a4d8d506f0ef00c79e042
Rutuj <tailorrutuj28@gmail.com>
2022-11-04 17:37:41 +0530

    add order by Customer

--- Routes/orders.js
+++ Routes/orders.js
@@ -12,7 +12,7 @@ const {
 const router = express.Router();
 const { protect } = require("../Middleware/auth");
 
-router.route("/").get(protect, getOrders);
+router.route("/").post(protect, getOrders);
 router.route("/insert").post(protect, insertupdate);
 router.route("/prices").get(protect, getAllClothingPrice);
 router.route("/addPrices").post(protect, InsertUpdateClothingPrice);
